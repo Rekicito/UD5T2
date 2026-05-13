58dfff604284157f1fec104f57538541aafd11db
Iker <ikerhf14@gmail.com>
2026-05-13 12:47:00 +0100

    Crear gestion basica de elementos

--- src/net/salesianos/ripadbaisor/servicios/ListaInteligente.java
+++ src/net/salesianos/ripadbaisor/servicios/ListaInteligente.java
@@ -1,5 +1,37 @@
 package net.salesianos.ripadbaisor.servicios;
 
+import java.util.ArrayList;
+import net.salesianos.ripadbaisor.modelo.Elemento;
+
 public class ListaInteligente {
-    
+
+    private ArrayList<Elemento> elementos;
+
+    public ListaInteligente() {
+        elementos = new ArrayList<>();
+    }
+
+    public void addElemento(Elemento elemento) {
+        elementos.add(elemento);
+    }
+
+    public String mostrarElementos() {
+
+        if (elementos.isEmpty()) {
+            return "No hay elementos guardados en la lista";
+        }
+
+        String resultado = "";
+
+        for (Elemento elemento : elementos) {
+
+            resultado += elemento.toString() + "\n";
+        }
+
+        return resultado;
+    }
+
+    public ArrayList<Elemento> getElementos() {
+        return elementos;
+    }
 }
