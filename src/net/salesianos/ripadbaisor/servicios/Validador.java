495f21b3ee146f4dcf0fa40e1a597a693684bea6
Iker <ikerhf14@gmail.com>
2026-05-13 12:58:12 +0100

    Añadir validacion de datos con JOptionPane

--- src/net/salesianos/ripadbaisor/servicios/Validador.java
+++ src/net/salesianos/ripadbaisor/servicios/Validador.java
@@ -1,5 +1,36 @@
 package net.salesianos.ripadbaisor.servicios;
 
+import javax.swing.JOptionPane;
+
 public class Validador {
-    
+
+    public static String pedirTexto(String mensaje) {
+
+        return JOptionPane.showInputDialog(mensaje);
+    }
+
+    public static int pedirNumero(String mensaje) {
+
+        int numero = 0;
+        boolean valido = false;
+
+        while (!valido) {
+
+            try {
+
+                numero = Integer.parseInt(
+                        JOptionPane.showInputDialog(mensaje));
+
+                valido = true;
+
+            } catch (NumberFormatException e) {
+
+                JOptionPane.showMessageDialog(
+                        null,
+                        "Debes introducir un número válido");
+            }
+        }
+
+        return numero;
+    }
 }
